8d45502736dbc5f5b1b7d8c55bc4b08c82343d6c
Jeff Ubri <jeffubri@gmail.com>
2022-01-02 10:53:55 -0500

    add google authUrl helper function

---
+++ auth/src/helper/googleOauth/authUrl.ts
@@ -0,0 +1,20 @@
+import { google } from "googleapis";
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "https:///auth/google"
+);
+
+function getGoogleAuthURL() {
+  const scopes = [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ];
+
+  return oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: scopes,
+  });
+}
