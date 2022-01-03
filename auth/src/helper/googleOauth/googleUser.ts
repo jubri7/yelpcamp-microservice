a1f0b0141cc7077c6ba52db6d3e1cf4ec5df878b
Jeff Ubri <jeffubri@gmail.com>
2022-01-03 10:30:42 -0500

    create generateError helper function

--- auth/src/helper/googleOauth/googleUser.ts
+++ auth/src/helper/googleOauth/googleUser.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "https:///auth/google"
+  "https:///sales.dev/api/users/"
 );
 
 export const getGoogleUser = async (code: string) => {
